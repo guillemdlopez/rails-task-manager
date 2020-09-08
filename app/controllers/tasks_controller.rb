41b862b9d44e36d07add4168ed8f96459a69b7c6
Guillem Delas <guillemdelas@hotmail.com>
2020-09-08 14:42:08 +0200

    Task manager challenge completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+
+    if @task.completed
+      @message = "The task is completed"
+    else
+      @message = "The task is not completed yet"
+    end
+    @message
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
